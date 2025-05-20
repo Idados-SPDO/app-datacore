a52aca01289852b67aa950f52dd1855b651f1361
FGV Idados_SPDO <idados_spdo@fgv.br>
2025-05-20 11:17:40 -0300

    Update details.py

--- components/details.py
+++ components/details.py
@@ -1,9 +1,10 @@
 import streamlit as st
+from config import get_session 
 from data.links import data_links
 from data.solutions import data_solutions
 from components.cards import multiple_cards_component
 from components.report import render_report, render_summary
-from utils.download import download_pdf
+from components.download_pdf_buttons import render_buttons
 
 @st.dialog("Detalhes da Base", width="large")
 def show_base_details(base_tag):
@@ -13,12 +14,12 @@ def show_base_details(base_tag):
         return
 
     st.title(f"Base: {base_data['name']}")
-    tabs = st.tabs(["Relatório", "Resumo", "Soluções", "Downloads"])
+    tabs = st.tabs(["Resumo", "Relatório", "Soluções", "Downloads"])
 
     with tabs[0]:
-        render_report(base_tag)
-    with tabs[1]:
         render_summary(base_tag)
+    with tabs[1]:
+        render_report(base_tag)
     with tabs[2]:
         sols = [sol for sol in data_solutions if sol["tag"] == base_tag]
         if sols:
@@ -26,19 +27,6 @@ def show_base_details(base_tag):
             multiple_cards_component(sols, columns=3)
         else:
             st.info("Nenhuma solução encontrada para esta base.")
+
     with tabs[3]:
-        col1, col2 = st.columns(2)
-        with col1:
-            st.download_button(
-                "Baixar Metadados",
-                data=download_pdf(),
-                file_name=base_data["links"][0]["sublinks"][0]["file_name"],
-                mime="application/pdf"
-            )
-        with col2:
-            st.download_button(
-                "Baixar Dicionário de Variáveis",
-                data=download_pdf(),
-                file_name=base_data["links"][1]["sublinks"][0]["file_name"],
-                mime="application/pdf"
-            )
+        render_buttons(base_tag)
